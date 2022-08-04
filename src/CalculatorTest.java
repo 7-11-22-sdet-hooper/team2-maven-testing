d1132c25ae6ae5ca7fa996af7a7a6a79cc0c706a
Robert Banks <seoinsighters@gmail.com>
2022-08-04 10:51:10 -0500

    Refactored Calculator class

---
+++ src/CalculatorTest.java
@@ -0,0 +1,12 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CalculatorTest {
+	@Test
+	public void shouldAddTwoNumbers() {
+		Calculator calculator = new Calculator();
+
+		Assert.assertEquals(calculator.add(2, 2), 4);
+	}
+	
+}
